729d764283a6720e61fa5cb2084106cd4f016ed1
schneems <richard.schneeman@gmail.com>
2012-01-16 06:34:51 -0600

    use url_for to improve wizard paths

--- lib/wicked/controller/concerns/path.rb
+++ lib/wicked/controller/concerns/path.rb
@@ -6,33 +6,22 @@ module Wicked::Controller::Concerns::Path
     wizard_path(@next_step)
   end
 
-  def current_url
-    request.url
+  def controller
+    params[:controller]
   end
 
-  def current_uri
-    URI.parse(current_url)
-  end
-
-  def base_uri
-    "#{current_uri.scheme}://#{current_uri.host}"
-  end
-
-  def current_path
-    current_uri.pat
-  end
-
-  def current_path_array
-    current_uri.path.split('/')
+  def action
+    params[:action]
   end
 
 
-  def wizard_path(goto_step = nil)
-    path_array = current_path_array
-    if goto_step.present?
-      path_array.pop if steps.map(&:to_sym).include? path_array.last.to_sym
-      path_array << goto_step.to_s
-    end
-    File.join('/', *path_array)
+  def wizard_path(goto_step = nil, options = {})
+    options = {
+                 :controller => controller,
+                 :action     => 'show',
+                 :id         => goto_step || action,
+                 :only_path  => true
+               }.merge options
+    url_for(options)
   end
 end
