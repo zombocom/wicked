ca66f9ca04949ea9401b8a1dab04fde10ffe765e
schneems <richard.schneeman@gmail.com>
2012-10-30 17:16:07 -0500

    debugging 1.8.7 on travis

--- test/integration/i18n_test.rb
+++ test/integration/i18n_test.rb
@@ -2,18 +2,25 @@ require 'test_helper'
 
 class I18nTest < ActiveSupport::IntegrationCase
 
-  test 'renders in english' do
-    step = :first
-    visit(i18n_path(step, :locale => :en))
-    assert has_content?('first')
-    assert has_link?('hello', :href => i18n_path(:second))
-
-  end
-
   test 'renders in spanish' do
     step = :uno
     visit(i18n_path(step, :locale => :es))
     assert has_content?('uno')
+    puts "=====================\n"
+    puts i18n_path(:dos).inspect
+    puts "^^^^^^^^^^^^^^^^^^^^^\n"
+    puts page.html
     assert has_link?('hello', :href => i18n_path(:dos))
+  end
+
+  test 'renders in english' do
+    step = :first
+    visit(i18n_path(step, :locale => :en))
+    assert has_content?('first')
+    puts "=====================\n"
+    puts i18n_path(:second).inspect
+    puts "^^^^^^^^^^^^^^^^^^^^^\n"
+    puts page.html
+    assert has_link?('hello', :href => i18n_path(:second))
   end
 end
