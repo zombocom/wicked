83021d4c7eac0af910a5d0740d3a5d980fb5567a
schneems <richard.schneeman@gmail.com>
2012-11-07 13:57:58 -0600

    refactor concerns/steps

--- lib/wicked/controller/concerns/steps.rb
+++ lib/wicked/controller/concerns/steps.rb
@@ -15,32 +15,32 @@ module Wicked::Controller::Concerns::Steps
 
   # will return true if step passed in is the currently rendered step
   def current_step?(step_name)
-    return false if step_name.nil? || step.nil?
+    return false if current_and_given_step_exists?(step_name)
     step == step_name
   end
 
   # will return true if the step passed in has already been executed by the wizard
   def past_step?(step_name)
-    return false if steps.index(step).nil? || steps.index(step_name).nil?
-    steps.index(step) > steps.index(step_name)
+    return false if current_and_given_step_exists?(step_name)
+    current_step_index > step_index_for(step_name)
   end
 
   # will return true if the step passed in has already been executed by the wizard
   def future_step?(step_name)
-    return false if steps.index(step).nil? || steps.index(step_name).nil?
-    steps.index(step) < steps.index(step_name)
+    return false if current_and_given_step_exists?(step_name)
+    current_step_index < step_index_for(step_name)
   end
 
   # will return true if the last step is the step passed in
   def previous_step?(step_name)
-    return false if steps.index(step).nil? || steps.index(step_name).nil?
-    steps.index(step) - 1  == steps.index(step_name)
+    return false if current_and_given_step_exists?(step_name)
+    (current_step_index - 1)  == step_index_for(step_name)
   end
 
   # will return true if the next step is the step passed in
   def next_step?(step_name)
-    return false if steps.index(step).nil? || steps.index(step_name).nil?
-    steps.index(step) + 1  == steps.index(step_name)
+    return false if current_and_given_step_exists?(step_name)
+    (current_step_index + 1)  == step_index_for(step_name)
   end
 
   module ClassMethods
@@ -78,6 +78,20 @@ module Wicked::Controller::Concerns::Steps
     step  = steps.at(index + 1) if index.present?
     step  ||= :finish
     step
+  end
+
+  private
+
+  def step_index_for(step_name)
+    steps.index(step_name)
+  end
+
+  def current_step_index
+    step_index_for(step)
+  end
+
+  def current_and_given_step_exists?(step_name)
+    return false if current_step_index.nil? || steps.index(step_name).nil?
   end
 
 
