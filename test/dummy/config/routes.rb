88e70be82cb730fda90cf172322a7ebc84bdbaca
schneems <richard.schneeman@gmail.com>
2012-11-29 16:52:09 -0600

    make test homepage dynamic

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -54,7 +54,7 @@ Dummy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#index"
 
   # See how all your routes lay out with "rake routes"
 
