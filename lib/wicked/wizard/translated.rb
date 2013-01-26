cdf033f6fb4ecc6766ff94c8f7c9187efa058fd8
schneems <richard.schneeman@gmail.com>
2013-01-26 14:35:52 -0600

    closes #51 fix step translation behavior
    Rather than be clever by translating some values and not others, `step` and the values in `steps` will now always be the same language. This means that now all the step helpers work correctly, but if someone needs to use a canonical step value in their controller such as using `step` with a case statement they must now use `wizard_value()` to cohere the value into the canonical value like this:
    
    ```
    case wizard_value(step)
    when :first
      # …
    when :second
      #…
    ```
    
    wizard_value will always return a value in the initially declared steps for example if the locale is set to spanish and steps are set to:
    
    ```
    steps :first, :second
    ```
    
    Then the value `wizard_value()` will only return a value of `:first` or `:second`:
    
    ```
    wizard_value(:uno) # => :first
    ```
    
    If the language is set to english it will still return the correct value:
    
    ```
    wizard_value(:first) # => :first
    ```
    
    The important thing to remember is that `step` and the values in `steps` are always going to be in the same language if you're using the Wicked translations. If you need any values to match the values set directly in your controller, or the names of your files (i.e. `views/#../first.html.erb`, then you need to use `wizard_value` method.

--- lib/wicked/wizard/translated.rb
+++ lib/wicked/wizard/translated.rb
@@ -2,28 +2,65 @@ module Wicked
   module Wizard
     module Translated
       extend ActiveSupport::Concern
+      include Wicked::Wizard
 
       included do
-        include Wicked::Wizard
         skip_before_filter :setup_wizard
         before_filter      :setup_wizard_translated
+
+        helper_method      :wizard_translate, :wizard_value
       end
 
       # creates a hash where keys are translated steps, values are the name of the view file
-      # {:first => "first", :second => "second"}
-      # {:uno   => "first",   :dos  => "second"}
+      #
+      #  es:
+      #    hello: "hola mundo"
+      #    wicked:
+      #      first: "uno"
+      #      second: "dos"
+      #
+      #   steps :first, :second
+      #
+      # {:uno   => :first, :dos    => :second} # spanish
+      # {:first => :first, :second => :second} # english
       #
       def wizard_translations
         @wizard_translations ||= steps.inject(ActiveSupport::OrderedHash.new) do |hash, step|
-          step        = step.to_s.split(".").last
-          translation = I18n.t("wicked.#{step}").to_sym
+          step              = step.to_s.split(".").last
+          translation       = wizard_translate(step)
           hash[translation] = step.to_sym
           hash
         end
       end
 
-      private
+      # takes a canonical wizard value and translates to correct language
+      #
+      # es.yml
+      # wicked:
+      #   first: "uno"
+      #
+      #   wizard_translate(:first) # => :uno
+      def wizard_translate(step_name)
+        I18n.t("wicked.#{step_name}", :raise => true).to_sym
+      rescue I18n::MissingTranslationData
+        # don't symbolize if key doesn't exist
+        I18n.t("wicked.#{step_name}")
+      end
 
+      # takes an already translated value and converts to a canonical wizard value
+      #
+      # es.yml
+      # wicked:
+      #   first: "uno"
+      #
+      #   wizard_value(step) # => :first
+      #
+      def wizard_value(step_name)
+        wizard_translations[step]
+      end
+
+
+      private
       # sets up a translated wizard controller
       # translations are expected under the 'wicked' namespace
       #
@@ -41,14 +78,13 @@ module Wicked
       #     steps "wicked.first", "wicked.second"
       #
       def setup_wizard_translated
-        self.steps     = wizard_translations.keys     # must come before setting previous/next steps
-        step_name      = setup_step_from(params[:id])
-        check_steps!(step_name)
-        @previous_step = previous_step(step_name)
-        @next_step     = next_step(step_name)
-        @step          = wizard_translations[step_name]  # translates step name to url
+        self.steps = wizard_translations.keys     # must come before setting previous/next steps
+        @step      = setup_step_from(params[:id])
+        check_steps!(@step)
+        set_previous_next(@step)
       end
       public
     end
   end
 end
+
