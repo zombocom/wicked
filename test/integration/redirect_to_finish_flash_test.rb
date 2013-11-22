8f3ab66836b3cdb605bc9dc798c5fb62a4b0dfb7
Szymon Szeliga <szymon.szeliga@kmpgroup.pl>
2013-11-22 18:39:09 +0100

    Added custom text to view, to verify it's the correct page

--- test/integration/redirect_to_finish_flash_test.rb
+++ test/integration/redirect_to_finish_flash_test.rb
@@ -5,6 +5,7 @@ class RedirectToFinishFlashTest < ActiveSupport::IntegrationCase
     step = :last_step
     visit(redirect_to_finish_flash_path(step))
     click_link 'Next'
+    assert has_content?('redirect_to_finish_flash/index')
     assert has_content?('Flash message notice')
   end
 end
