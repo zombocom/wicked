f276d503c253d5388e4848070ccf9054580ef618
Matt Connolly <matt.connolly@tworedkites.com>
2014-05-29 20:59:54 +1000

    Update controller test, does it work in Rails 3?

--- test/controllers/updates_controller_test.rb
+++ test/controllers/updates_controller_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class UpdatesControllerTest < ActionController::TestCase
   test 'redirect from first step to second step' do
-    put :update, :id => :first
+    put :update, :id => 'first'
     assert_redirected_to update_path(:second)
     assert_equal 'Thing was updated from step first.', flash[:notice]
   end
 
   test 'redirect from second step to final step' do
-    put :update, :id => :second
+    put :update, :id => 'second'
     assert_redirected_to update_path(:last_step)
     assert_equal 'Thing was updated from step second.', flash[:notice]
   end
 
   test 'redirect from last_step to root path' do
-    put :update, :id => :last_step
+    put :update, :id => 'last_step'
     assert_redirected_to update_path(:wicked_finish)
     assert_equal 'Thing was updated from step last_step.', flash[:notice]
   end
