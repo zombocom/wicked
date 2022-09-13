a46ffb8a1f23aabef0d003dd3c632315373a38b6
Deepak Mahakale <deepakmahakale@gmail.com>
2022-09-13 09:38:43 +0530

    Add rails 7.0 to CI (#293)
    Add 7.0 to Appraisal
    Fix issue in the test helper for rails 7

--- Appraisals
+++ Appraisals
@@ -1,11 +1,11 @@
-appraise "rails-5.1" do
-  gem "rails", "~> 5.2"
-end
-
 appraise "rails-5.2" do
   gem "rails", "~> 5.2"
 end
 
-appraise "rails-6.0.1" do
-  gem "rails", "~> 6.0.1"
+appraise "rails-6.0" do
+  gem "rails", "~> 6.0"
+end
+
+appraise "rails-7.0" do
+  gem "rails", "~> 7.0"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,12 +18,12 @@ Capybara.default_selector = :css
 
 # https://github.com/plataformatec/devise/blob/master/test/orm/active_record.rb
 migrate_path = File.expand_path("../rails_app/db/migrate", __FILE__)
-if Rails.version.start_with? '6'
-  ActiveRecord::MigrationContext.new(migrate_path, ActiveRecord::SchemaMigration).migrate
-elsif Rails.version.start_with? '5.2'
+if ActiveRecord.version < Gem::Version.create("5.2.0")
+  ActiveRecord::Migrator.migrate(migrate_path)
+elsif ActiveRecord.version < Gem::Version.create("6.0.0")
   ActiveRecord::MigrationContext.new(migrate_path).migrate
 else
-  ActiveRecord::Migrator.migrate(migrate_path)
+  ActiveRecord::MigrationContext.new(migrate_path, ActiveRecord::SchemaMigration).migrate
 end
 
 # Load support files
